32f8dee488748b8b6380e3ad11f668d4a658c1e6
DarkMaster7121 <67087425+Dark7121@users.noreply.github.com>
2023-12-04 10:35:05 +0000

    Modified build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,13 @@
 echo = "BUILD START"
 
+wget https://www.sqlite.org/2023/sqlite-autoconf-3360000.tar.gz
+tar xvfz sqlite-autoconf-3360000.tar.gz
+cd sqlite-autoconf-3360000
+./configure
+make
+make install
+cd ..
+
 yum install -y sqlite-devel
 
 pip install --no-cache-dir -r requirements.txt
