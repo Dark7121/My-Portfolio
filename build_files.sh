36f45fd9552e6bb17f4605a2028721a7ad0cfaba
DarkMaster7121 <67087425+Dark7121@users.noreply.github.com>
2023-12-04 08:04:48 +0000

    build file update 0.045

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
 echo = "BUILD START"
+
+apt-get update
+apt-get install -y libsqlite3-dev
+
 pip install --no-cache-dir -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
+
 echo = "BUILD END"
