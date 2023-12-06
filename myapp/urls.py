459a8777e58ece96c7d0d9221d902219f828e78d
DarkMaster7121 <67087425+Dark7121@users.noreply.github.com>
2023-12-06 01:52:42 +0000

    Admin setup done

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('project/', views.my_project, name='project'),
+    path('contact-me/', views.contact_me, name='contact-me'),
+    path('project/contact-me', views.contact_me, name='contact-me'),
+    path('contact-me/project', views.my_project, name='project'),
+    path('contact-me/contact-me', views.contact_me, name='contact-me'),
+    path('project/project', views.my_project, name='project'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,9 @@ import requests
 
 def home(request):
     return render(request, 'home.html')
+
+def my_project(request):
+    return render(request, 'my_projects.html')
+
+def contact_me(request):
+    return render(request, 'contact_me.html')
